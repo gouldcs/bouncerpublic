eca20cc5b1f234e34b125225148ab4663afc26a7
Cameron Gould <56109943+gouldcs@users.noreply.github.com>
2020-09-08 01:16:17 -0700

    Bouncer Bot

--- BouncerBot.py
+++ BouncerBot.py
@@ -59,28 +59,7 @@ client = Bot(command_prefix="BOT_PREFIX")
     https://developers.google.com/gmail/api/quickstart/python
 """
 def email_setup():
-    creds = None
-    # The file token.pickle stores the user's access and refresh
-    # tokens, and is created automatically when the authorization
-    # flow completes for the first time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log
-    # in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('gmail', 'v1', credentials=creds)
-    return service
+   return false
 
 """The authorized email client"""
 SERVICE = email_setup()
